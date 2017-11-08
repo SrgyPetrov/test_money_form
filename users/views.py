af4b2f605731394f3ca65b949c8b677241e7e1b2
Sergey Petrov <desoulate777@gmail.com>
2017-11-08 15:24:18 +0300

    Fix autocomplete view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework.generics import ListAPIView
 
 from .models import User
@@ -6,5 +7,11 @@ from .serializers import UserSerializer
 
 class UserList(ListAPIView):
 
-    queryset = User.objects.filter(is_active=True, is_superuser=False, is_staff=False)
     serializer_class = UserSerializer
+
+    def get_queryset(self):
+        qs = User.objects.filter(is_active=True, is_superuser=False, is_staff=False)
+        query = self.request.query_params.get('query', None)
+        if query is not None:
+            qs = qs.filter(Q(inn__contains=query) | Q(username__contains=query))
+        return qs
